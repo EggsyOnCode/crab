11dec98e403837418329e45bceabf51f4e7860ce
EggsyOnCode <farzansaqib2003@gmail.com>
2024-09-29 01:49:17 +0500

    terminal realted code migraated to separeate module and gui basic made

--- src/editor.rs
+++ src/editor.rs
@@ -2,8 +2,8 @@ use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, 
 use crossterm::execute;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use std::io::stdout;
-
-
+mod terminal;
+use terminal::Terminal;
 pub struct Editor {
     should_quit : bool,
 }
@@ -13,32 +13,29 @@ impl Editor {
         Editor { should_quit: false }
     }
     pub fn run(&mut self) {
-        Self::initialize().unwrap();
+        Terminal::initialize().unwrap();
         let result = self.repl();
-        Self::terminate().unwrap();
+        Terminal::terminate().unwrap();
         result.unwrap();
     }
 
-    fn initialize() -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-        Self::clear_screen()
-    }
-    fn terminate() -> Result<(), std::io::Error> {
-        disable_raw_mode()
-    }
-    fn clear_screen() -> Result<(), std::io::Error> {
-        let mut stdout = stdout();
-        execute!(stdout, Clear(ClearType::All))
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let height = Terminal::size()?.1;
+        for r in 0..height {
+            crossterm::cursor::MoveTo(0,r);
+            println!("\r~")
+        }
+        Ok(())
     }
 
     fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            let event = read()?;
-            self.evaluate_event(&event);
             self.refresh_screen()?;
             if self.should_quit {
                 break;
             }
+            let event = read()?;
+            self.evaluate_event(&event);
         }
         Ok(())
     }
@@ -57,8 +54,11 @@ impl Editor {
     }
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         if self.should_quit {
-            Self::clear_screen()?;
+            Terminal::clear_screen()?;
             print!("Goodbye.\r\n");
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(0, 0)?;
         }
         Ok(())
     }
---
+++ src/editor/terminal.rs
@@ -0,0 +1,30 @@
+use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, Event};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, size};
+use std::io::stdout;
+use crossterm::cursor::MoveTo;
+
+pub struct Terminal {}
+
+impl Terminal {
+    pub fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+    pub fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+    pub fn initialize() -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()?;
+        Self::move_cursor_to(0, 0)?;
+        Ok(())
+    }
+    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), std::io::Error> {
+        execute!(stdout(), MoveTo(x, y))?;
+        Ok(())
+    }
+    pub fn size() -> Result<(u16, u16), std::io::Error> {
+        return size();
+    }
+}
