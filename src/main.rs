a27c4341a1d2789ae482e55d796b4c0e37c3d1e2
EggsyOnCode <farzansaqib2003@gmail.com>
2024-09-29 00:17:45 +0500

    reading input and key presses as bin chars and processing them

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use std::io::{self, Read};
+use std::io::{self, Read, };
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 fn main() -> std::io::Result<()> {
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        print!("{}", c);
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("Binary: {0:08b} ASCII: {0:#03}\r", b);
+        } else {
+            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+        }
         if c == 'q' {
             disable_raw_mode().unwrap();
             break;
         }
     }
-    
     Ok(())
 }
